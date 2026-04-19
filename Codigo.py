f134ecc46925e76ccfafbc1450a24c2c6a712ac8
angiebarron23 <angie.cabello16@gmail.com>
2026-04-19 15:56:04 -0600

    se creo init

---
+++ Codigo.py
@@ -0,0 +1,8 @@
+class CuentaBancaria:
+    def __init__(self, numero_cuenta, titular, saldo, tipo_cuenta, activa) :
+        self.__numero_cuenta=numero_cuenta
+        self.__titular=titular
+        self.__saldo=saldo
+        self.__tipo_cuenta=tipo_cuenta
+        self.__activa=activa 
+        
