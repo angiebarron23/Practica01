8ac263891a386a3beedf2373bc185482da94f3d1
angiebarron23 <angie.cabello16@gmail.com>
2026-04-19 16:35:34 -0600

    se agregaron los getters de la clase cuenta bancaria

--- Codigo.py
+++ Codigo.py
@@ -1,8 +1,25 @@
 class CuentaBancaria:
-    def __init__(self, numero_cuenta, titular, saldo, tipo_cuenta, activa) :
-        self.__numero_cuenta=numero_cuenta
-        self.__titular=titular
-        self.__saldo=saldo
-        self.__tipo_cuenta=tipo_cuenta
-        self.__activa=activa 
-        
+ def __init__(self, numero_cuenta, titular, saldo, tipo_cuenta, activa):
+   self.__numero_cuenta=numero_cuenta
+   self.__titular=titular
+   self.__saldo=saldo
+   self.__tipo_cuenta=tipo_cuenta
+   self.__activa=activa
+
+ #GETTERS 
+ def get_numero_cuenta(self):
+    return self.__numero_cuenta
+
+def get_titular(self):
+    return self.__titular
+
+def get_saldo(self):
+    return self.__saldo
+
+def get_tipo_cuenta(self):
+    return self.__tipo_cuenta
+
+def get_activa(self):
+    return self._activa 
+
+
